2d1d21e04d508dd67ff03f79a2bda110ca3d15c4
Dhruvgarg91 <dhruv.g4445@gmail.com>
2023-08-05 23:39:08 +0530

    update

--- Scripts/Models/model.js
+++ Scripts/Models/model.js
@@ -1,36 +1,9 @@
 class MusicTrack {
   constructor(data) {
-      this.wrapperType = data.wrapperType;
-      this.kind = data.kind;
-      this.artistId = data.artistId;
-      this.collectionId = data.collectionId;
-      this.trackId = data.trackId;
-      this.artistName = data.artistName;
-      this.collectionName = data.collectionName;
-      this.trackName = data.trackName;
-      this.collectionCensoredName = data.collectionCensoredName;
-      this.trackCensoredName = data.trackCensoredName;
-      this.artistViewUrl = data.artistViewUrl;
-      this.collectionViewUrl = data.collectionViewUrl;
-      this.trackViewUrl = data.trackViewUrl;
-      this.previewUrl = data.previewUrl;
-      this.artworkUrl30 = data.artworkUrl30;
-      this.artworkUrl60 = data.artworkUrl60;
-      this.artworkUrl100 = data.artworkUrl100;
-      this.collectionPrice = data.collectionPrice;
-      this.trackPrice = data.trackPrice;
-      this.releaseDate = new Date(data.releaseDate);
-      this.collectionExplicitness = data.collectionExplicitness;
-      this.trackExplicitness = data.trackExplicitness;
-      this.discCount = data.discCount;
-      this.discNumber = data.discNumber;
-      this.trackCount = data.trackCount;
-      this.trackNumber = data.trackNumber;
-      this.trackTimeMillis = data.trackTimeMillis;
-      this.country = data.country;
-      this.currency = data.currency;
-      this.primaryGenreName = data.primaryGenreName;
-      this.isStreamable = data.isStreamable;
+    this.artistName = data.artistName;
+    this.trackName = data.trackName;
+    this.previewUrl = data.previewUrl;
+    this.artworkUrl100 = data.artworkUrl100;
   }
 }
 export default MusicTrack;
--- Scripts/Services/product_operations.js
+++ Scripts/Services/product_operations.js
@@ -7,37 +7,10 @@ export default async function readAllProducts() {
     const music = obj["results"];
     const musicArray = music.map((musicData) => {
       const musicTrack = new MusicTrack({
-        wrapperType: musicData.wrapperType,
-        kind: musicData.kind,
-        artistId: musicData.artistId,
-        collectionId: musicData.collectionId,
-        trackId: musicData.trackId,
         artistName: musicData.artistName,
-        collectionName: musicData.collectionName,
         trackName: musicData.trackName,
-        collectionCensoredName: musicData.collectionCensoredName,
-        trackCensoredName: musicData.trackCensoredName,
-        artistViewUrl: musicData.artistViewUrl,
-        collectionViewUrl: musicData.collectionViewUrl,
-        trackViewUrl: musicData.trackViewUrl,
         previewUrl: musicData.previewUrl,
-        artworkUrl30: musicData.artworkUrl30,
-        artworkUrl60: musicData.artworkUrl60,
         artworkUrl100: musicData.artworkUrl100,
-        collectionPrice: musicData.collectionPrice,
-        trackPrice: musicData.trackPrice,
-        releaseDate: musicData.releaseDate,
-        collectionExplicitness: musicData.collectionExplicitness,
-        trackExplicitness: musicData.trackExplicitness,
-        discCount: musicData.discCount,
-        discNumber: musicData.discNumber,
-        trackCount: musicData.trackCount,
-        trackNumber: musicData.trackNumber,
-        trackTimeMillis: musicData.trackTimeMillis,
-        country: musicData.country,
-        currency: musicData.currency,
-        primaryGenreName: musicData.primaryGenreName,
-        isStreamable: musicData.isStreamable,
       });
       return musicTrack;
     });
