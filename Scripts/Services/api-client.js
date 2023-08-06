3f9f44d4bdf92748333af944dbf2f81b1b71f9dc
Dhruvgarg91 <dhruv.g4445@gmail.com>
2023-08-06 12:59:29 +0530

    json updated

--- Scripts/Services/api-client.js
+++ Scripts/Services/api-client.js
@@ -1,6 +1,6 @@
 export default async function doNetworkCall() {
   const URL =
-    "https://gist.githubusercontent.com/Dhruvgarg91/be210cc5f39cfbb51dfca5a6be59d998/raw/0945b7f8182e6a143dd13753442786855c3addec/music.json";
+    "https://gist.githubusercontent.com/Dhruvgarg91/be210cc5f39cfbb51dfca5a6be59d998/raw/c01cdc7b09c431c00b683608e3bc470aaac2210d/music.json";
   try {
     const response = await fetch(URL);
     const object = await response.json();
